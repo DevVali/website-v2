d4c391e6b71ba0bc07982045389ce95ea13755f8
DevVali <90271108+DevVali@users.noreply.github.com>
2022-02-16 19:33:51 +0100

    Update songinfoint.js

--- codes/music/js/songinfoint.js
+++ codes/music/js/songinfoint.js
@@ -17,7 +17,6 @@ $c[ Credits of the code go to Berk#3506 and vidk#2010]
 $c[ API by Almighty Neko#0121 ]
 
 $nomention
-$ephemeral
 
 $var[host;https://bdfd-music.YOUR-REPLIT-USERNAME.repl.co] $c[<=== REPLACE THE HOST WITH YOUR OWN URL] 
 $c[P.S. this is a url you entered in the uptimerobot.com dashboard, such as https://bdfd-music.your-replit-username.repl.co]
