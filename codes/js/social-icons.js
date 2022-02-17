89ca61e615a3285935bb54af33dd9758b1184ab2
DevVali <90271108+DevVali@users.noreply.github.com>
2022-02-17 08:09:12 +0100

    Create social-icons.js

---
+++ codes/js/social-icons.js
@@ -0,0 +1,7 @@
+document.getElementById("code2").value = 
+            
+`<a href="https://discord.com/users/773911690629742602"><button style="background-color:#5865F2"><i class="fab fa-discord" style="font-size:25px;color:#141414"></i></button></a>
+<a href="https://github.com/DevVali"><button style="background-color:#ebecec"><i class="fab fa-github" style="font-size:25px;color:#141414"></i></button></a>
+<a href="https://open.spotify.com/user/mkngmq9pvi5krjmmuuzf8uulg?si=0741b048dc724e9a"><button style="background-color:#1DB954"><i class="fab fa-spotify" style="font-size:25px;color:#141414"></i></button></a>
+<a href="https://twitter.com/ejvali"><button style="background-color:#1DA1F2"><i class="fab fa-twitter" style="font-size:25px;color:#141414"></i></button></a>`
+;
