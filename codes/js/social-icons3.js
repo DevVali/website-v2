7a427ec60d6f80f4c4ed9c9d3e36bafc03f402e1
DevVali <90271108+DevVali@users.noreply.github.com>
2022-02-17 08:09:29 +0100

    Create social-icons3.js

---
+++ codes/js/social-icons3.js
@@ -0,0 +1,58 @@
+document.getElementById("code3").value = 
+            
+`<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            Social Icons Preview
+        </title>
+        <style>
+            body {
+                background-color: #141414;
+                text-align: center;
+            }
+
+            h1 {
+                color: #5865F2;
+                font-family: Verdana, sans-serif;
+            }
+
+            h3 {
+                color: #894EFF;
+                font-family: 'Courier New', Courier, monospace;
+            }
+
+            p {
+                color: #ebecec;
+                font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            }
+
+            button {
+                height: 100px;
+                width: 100px;
+                height: 50px;
+                border-radius: 100px;
+                border: solid 5px;
+                margin-top: 8px;
+                margin-left: 2x;
+                margin-right: 2px;
+                background-color: #894EFF;
+                color:black;
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+                cursor: pointer;
+                }
+        </style>
+        <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <h1>Social Icons</h1>
+        <br>
+        <a href="https://discord.com/users/773911690629742602"><button style="background-color:#5865F2"><i class="fab fa-discord" style="font-size:25px;color:#141414"></i></button></a>
+        <a href="https://github.com/DevVali"><button style="background-color:#ebecec"><i class="fab fa-github" style="font-size:25px;color:#141414"></i></button></a>
+        <a href="https://open.spotify.com/user/mkngmq9pvi5krjmmuuzf8uulg?si=0741b048dc724e9a"><button style="background-color:#1DB954"><i class="fab fa-spotify" style="font-size:25px;color:#141414"></i></button></a>
+        <a href="https://twitter.com/ejvali"><button style="background-color:#1DA1F2"><i class="fab fa-twitter" style="font-size:25px;color:#141414"></i></button></a>
+    </body>
+</html>`
+;
