af4a125b46ec0fa2c7e3077ae202868b7fdb8c40
DevVali <90271108+DevVali@users.noreply.github.com>
2022-03-11 20:06:31 +0100

    Create theme1.js

---
+++ codes/js/theme1.js
@@ -0,0 +1,22 @@
+document.getElementById("code1").value = 
+            
+`<!doctype html>
+<html> 
+ <head> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> 
+  <title>Dark and Light Mode</title> 
+ </head> 
+ <body id="body"> 
+  <br> 
+  <h3>Dark and Light Mode</h3> 
+  <p>Made by vidk#2010<br>You can use it without credits.<br>Don't claim them yourself.</p> 
+  <br><button class="btn btn-primary" onclick="changeToLight();" id="light">Light Mode</button> <button class="btn btn-primary" onclick="changeToDark();" id="dark">Dark Mode</button> 
+  <br> 
+  <br> 
+  <footer>
+    Vali © 2022 
+  </footer> 
+ </body>
+</html>`
+;
